d08daf7def8c475dccce37b8408e3811671a5e59
Mike Chau <mchau@iwu.edu>
2013-09-03 02:13:58 -0500

    Update to 3.11

--- upgrade_kernel.sh
+++ upgrade_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #script based on the one found on upbuntu.com
 
-echo "$(tput setaf 3)--- Kernel 3.10.10 will be installed in an `uname -i` system---$(tput sgr0)"
+echo "$(tput setaf 3)--- Kernel 3.11 will be installed in an `uname -i` system---$(tput sgr0)"
 
 echo ""
 
